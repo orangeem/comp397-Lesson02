546c4e7a1cda873edcdeb21fe9b58a686c0f008f
orangeem <orangeem@naver.com>
2015-01-12 13:42:32 -0500

    created basic site structure

---
+++ comp397-Lesson02/Scripts/game.js
@@ -0,0 +1,13 @@
+﻿var Player = (function () {
+    function Player() {
+        this.strength = 10;
+    }
+    Player.prototype.fights = function () {
+        console.log("He Fights");
+    };
+    return Player;
+})();
+
+var tom = new Player();
+tom.fights();
+//# sourceMappingURL=game.js.map
---
+++ comp397-Lesson02/Scripts/game.ts
@@ -0,0 +1,13 @@
+﻿class Player {
+    strength: number;
+    constructor() {
+        this.strength = 10;
+    }
+
+    fights() {
+        console.log("He Fights");
+    }
+}
+
+var tom = new Player();
+tom.fights();
